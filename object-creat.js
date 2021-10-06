4e8f6bf42464fe40cac336060c78757ef8a392b9
sahhebali <saheb.co21@gmail.com>
2021-10-06 14:50:15 +0530

    js-object-concepts

--- object-creat.js
+++ object-creat.js
@@ -1,1 +1,28 @@
-console.log('check object creat concepts');
+// 1. using object letarel
+const student = { name: 'Md saheb', job: 'learn webdeveloer' };
+
+// 2. constructor 
+const person = new Object();
+
+// 3. 
+// const human = Object.create(null);
+const human = Object.create(student);
+// console.log(human.job);
+
+// class 
+class People {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const peop = new People('manus', 12);
+console.log(peop);
+
+function manus(name) {
+    this.name = name;
+}
+
+const man = new manus('kader');
+// console.log(man);
+
